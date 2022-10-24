bcb7358b886964f98e0ba4d871509249f8320b20
nmy6452 <nmy6452@naver.com>
2022-10-24 14:18:30 +0900

    FEAT: 월별 본회의 출력

--- src/main/java/com/example/nac/Mapper/commschemapper.java
+++ src/main/java/com/example/nac/Mapper/commschemapper.java
@@ -1,6 +1,5 @@
 package com.example.nac.Mapper;
 
-import com.example.nac.model.Scadule;
 import com.example.nac.model.commsche;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -9,7 +8,7 @@ import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-//DB에서 본회의 정보를 읽어와 본회의 모델 객체에 맵핑
+//DB에서 정기회의 정보를 읽어와 정기회의 모델 객체에 맵핑
 @Mapper
 public interface commschemapper {
 
@@ -19,7 +18,11 @@ public interface commschemapper {
     commsche GetcommscheTitle(@Param("TITLE") String TITLE);
 
     @Select("SELECT * FROM project2022.commsche where MEETING_DATE = #{date}")
-    List<Scadule> GetcommscheDate(@Param("date") String date);
+    List<commsche> GetcommscheDate(@Param("date") String date);
+
+    /* -------------------------------*/
+    @Select("SELECT * FROM project2022.commsche where MEETING_DATE like '%${month}%'")
+    List<commsche> GetcommscheMonth(@Param("month") String month);
 
     @Insert("INSERT INTO commsche VALUES(#{meetingsession}, #{cha}, #{title}, #{meeting_date}, #{meeting_time}, #{link_url}, #{unit_cd}, #{unit_nm})")
     int Putcommsche(@Param("meetingsession") String meetingsession,@Param("cha") String cha,@Param("title") String title, @Param("meeting_date") String meeting_date, @Param("meeting_time") String meeting_time, @Param("link_url") String link_url, @Param("unit_cd") String unit_cd, @Param("unit_nm") String unit_nm);
